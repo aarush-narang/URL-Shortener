9c8a945e5ebd0e74edb5c74bdbde501058a0464d
phantom-requiem <aarushnarang@gmail.com>
2021-09-18 22:41:37 -0700

    fixed bug with link validation

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, json, render_template, redirect, request
+from flask import Blueprint, json, render_template, redirect, request, jsonify
 import pymongo
 import json
 import hashlib
@@ -10,7 +10,7 @@ client = pymongo.MongoClient(MONGO_DB_URI)  # you could also use sql db for this
 
 db = client.url_shortener  # url_shortener is database name
 
-def short_link(link, char_length=6):
+def short_link(link, char_length=7):
     if char_length > 128:
         raise ValueError(f'char_length {char_length} exceeds 128')
     hash_object = hashlib.sha512(link.encode())
@@ -40,15 +40,30 @@ def existing_link(short_link):
 
 @router.post('/url_shorten')
 def url_shorten():
-    link_size = 6  # the number of characters at the end of the link example.com/thisstuff
+    link_size = 7  # the number of characters at the end of the link example.com/thisstuff
     data = request.data.decode()  # request data, the link they want to shorten
     link = json.loads(data)['link']  # convert from json to dict
+
+    # check if link is valid
+    if re.search('((http:\/\/)?127\.0\.0\.1:5000\/).+', link):
+        return jsonify(valid='invalidURL', msg='That is already a shortened link!')
+
+    full_link_regex = "((http|https):\/\/)(www.)?[a-zA-Z0-9@:%._\+~#?&\/=]{2,256}\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\+~#?&\/=]*)"
+    if not re.match(full_link_regex, link):
+        if '.' not in link:
+            return jsonify(valid='invalidURL', msg='Please enter a valid URL!')
+        if not re.search('(http|https)', link): # this can still be evaded, fix this
+            link = 'https://' + link
+        else:
+            return jsonify(valid='invalidURL', msg='Please enter a valid URL!')
+    
+
     short_url = short_link(link, link_size)  # shorten the link
 
-    short_link_check = db.urls.find({'short_link': short_url})  # check if link is already in db
+    short_link_check = db.urls.find({'short_link': short_url})  # check if short link is already in db
     for db_link in short_link_check:  # if link already in db
         if link in db_link['link'] :  # check if the db link is the same they want
-            return db_link['short_link']  #  if so return that short link
+            return jsonify(short_link=db_link['short_link'])  #  if so return that short link
         else:
             while True:  # otherwise keep generating short links with 1 more character until it gets one not in the db
                 new_link = short_link(link, link_size+1)
@@ -57,18 +72,14 @@ def url_shorten():
                     if db_link['link'] != link:
                         link_size += 1
                         continue
-                return new_link
+                return jsonify(short_link=new_link)
 
 
-    link_check = db.urls.find({'link': link})
+    link_check = db.urls.find({'link': link}) # check if link is already in db
     for link in link_check:
-        return link['short_link']
-
-    link_regex_pattern = 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)'
-    if not re.match(link_regex_pattern, link):
-        link = 'https://' + link
+        return jsonify(short_link=link['short_link'])
 
     db.urls.insert_one(
-        { 'link': link, 'short_link': short_url}
+        { 'link': link, 'short_link': short_url }
     )
-    return short_url
+    return jsonify(short_link=short_url)
